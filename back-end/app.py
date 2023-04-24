5c1a44be80443875f34b8dcac5b69f05771de8f7
Ivan Fuentes <consultant-ifuentes@remine.com>
2023-04-24 13:44:11 -0600

    Add plants route

--- back-end/app.py
+++ back-end/app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask import request
+from classifyPlants import classify_image
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, world!</p>"
+@app.route('/plants/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'plant_image' not in request.files:
+            return {
+                "success": "false",
+                "error": "No file part"
+            }
+    
+        f = request.files['plant_image']
+        pred = classify_image(f)
+        return {
+            "success": "true",
+            "prediction": pred,
+            "error": "None"
+        }
