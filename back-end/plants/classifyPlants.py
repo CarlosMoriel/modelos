03a9f6e1e43ba78fb51e3d78544f78d0e4910f61
Ivan Fuentes <ivan.fuentes.r@outlook.com>
2023-05-03 00:13:00 -0500

    greater threshold and add dict and max prob

--- back-end/plants/classifyPlants.py
+++ back-end/plants/classifyPlants.py
@@ -9,15 +9,15 @@ learn2 = load_learner('plants/plantsModel.pkl')
 
 categories = ('Aptenia cordifolia', 'aloe vera', 'epipremnum aureum', 'sansevieria trifasciata', 'spathiphyllum')
 
-def classify_image(img, threshold=0.8):
+def classify_image(img, threshold=0.7):
     imgToPredict = plt.imread(img)
     pred = learn2.predict(imgToPredict)
     pred_probs_dec = [f"{p:.20f}" for p in pred[2].tolist()]
     max_prob = max(pred[2].tolist())
     if max_prob >= threshold:
-        return pred[0], pred_probs_dec
+        return pred[0], dict(zip(categories, pred_probs_dec)), max_prob
     else:
-        return "Uncertain", pred_probs_dec
+        return "Uncertain", dict(zip(categories, pred_probs_dec)), max_prob
 
 def getClassesForLearner(learner):
     return learner.dls.vocab
